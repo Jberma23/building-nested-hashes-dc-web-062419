4f947113d359c121cab21abeeeac119a94b0030f
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-18 20:57:25 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,15 +1,15 @@
 def third_challenge
   epic_tragedy = {
      :montague => {
-        :patriarch => {name: "Lord Montague", age: "53"},
-        :matriarch => {name: "Lady Montague", age: "54"},
-        :hero => {name: "Romeo", age: "15", status: "alive"},
+        :patriarch => {name: , age:},
+        :matriarch => {name: , age: },
+        :hero => {name: , age: , status: },
         :hero_friends => []
      },
      :capulet => {
-        :patriarch => {name: "Lord Capulet", age: "50"},
-        :matriarch => {name: "Lady Capulet", age: "51"},
-        :heroine => {name: "Juliet", age: "15", status: "alive"},
+        :patriarch => {name: , age: },
+        :matriarch => {name: , age: },
+        :heroine => {name: , age: , status: },
         :heroine_friends => []
      }
   }
