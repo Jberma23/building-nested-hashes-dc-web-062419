129dbeaa6fa3f0852aec5990a2cef356f8f16c73
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-18 20:40:18 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,26 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => {}
+      :patriarch => {
+        :name => "Lord Montague",
+        :age => "53"
       },
+      :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
+      },
+      :hero => {
+        :name => "Romeo"
+        :age => "15"
+      },
+      :hero_friends => [
+        {
+          :name => "Benvolio",
+         :age => "17",
+         :attitude => "worried"
+        }
+      ]
+
     :capulet => {
       #your key/value pairs here!
       }}
